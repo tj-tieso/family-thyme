dd39bd9aa0afe479552a15cb34ac7e48ed11426d
Kyle Reynolds <kbreyn06@gmail.com>
2022-08-22 23:27:06 -0500

    drag and drop calendar events working

--- client/src/pages/Calendar.js
+++ client/src/pages/Calendar.js
@@ -3,20 +3,24 @@ import FullCalendar from '@fullcalendar/react' // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 import interactionPlugin from "@fullcalendar/interaction" // needed for dayClick
 
+
+
 export default class DemoApp extends React.Component {
 
   render() {
     return (
+      
       <FullCalendar
         plugins={[ dayGridPlugin, interactionPlugin ]}
         dateClick={this.handleDateClick}
+        editable={true}
+        
         events={[
-          { title: 'event 1 Kyle', date:'2022-08-24'}
+          { title: 'event 1', date: '2022-08-24' },
+          { title: 'event 2', date: '2022-08-26' }
         ]}
       />
     )
   }
-
- 
 
 }
