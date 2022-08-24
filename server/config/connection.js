e7fc42ade6edb4ce144e1d71086da17b29cad87d
tj-tieso <tj.tieso@gmail.com>
2022-08-24 16:19:11 -0500

    renamed server

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/family-thyme-test',
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/family-thyme',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
