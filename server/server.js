dffea742e2787f88202d6458b45eb0c412c4d196
tj-tieso <tj.tieso@gmail.com>
2022-08-24 14:54:14 -0500

    change app.get path to ../client/build/public/index.html

--- server/server.js
+++ server/server.js
@@ -7,12 +7,14 @@ const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
-const app = express();
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
+
+const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -25,7 +27,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/public/index.html'));
 });
 
 // Create a new instance of an Apollo server with the GraphQL schema
