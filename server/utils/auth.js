daeb1b365725b430baf21defecabbd745c3b22f5
tj-tieso <tj.tieso@gmail.com>
2022-08-24 09:30:12 -0500

    dotenv added. Secret locked in a grimoire of unknowable danger

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 const jwt = require('jsonwebtoken');
 
 // TODO: move to env
-const secret = 'mysecretsshhhhh';
+// const secret = 'mysecretsshhhhh';
 const expiration = '2d';
 
 // middleware
