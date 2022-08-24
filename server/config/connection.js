5bbf91e4b2725d1d87c3fb416528af5ba2912582
tj-tieso <tj.tieso@gmail.com>
2022-08-24 12:24:07 -0500

    changed db name

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/family-thyme",
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/family-thyme-test',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
